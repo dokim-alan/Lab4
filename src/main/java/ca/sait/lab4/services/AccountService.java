4558675f2914c97b9b696d2cdd773710f8529a0d
dokim-alan <dokim.alan@gmail.com>
2022-02-08 09:59:47 -0700

    First publish
    First publish

---
+++ src/main/java/ca/sait/lab4/services/AccountService.java
@@ -0,0 +1,19 @@
+package ca.sait.lab4.services;
+
+import ca.sait.lab4.models.User;
+
+/**
+ *
+ * @author USER
+ */
+public class AccountService {
+        public User login(String username, String password) {
+            if (username.equals("abe") && password.equals("password")) {
+                return new User(username, null);
+            } else if (username.equals("barb") && password.equals("password")) {
+                return new User(username, null);
+            } else {
+                return null;
+            }
+        }
+}
---
+++ src/main/java/ca/sait/lab4/servlets/LoginServlet.java
@@ -0,0 +1,82 @@
+package ca.sait.lab4.servlets;
+
+import ca.sait.lab4.models.User;
+import ca.sait.lab4.services.AccountService;
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Alan(Dong O) Kim
+ */
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();
+        
+        if ( session.getAttribute("username") != null) {
+            String query = request.getQueryString();
+            if (query != null && query.contains("logout")) {
+                session.invalidate();
+                 
+                request.setAttribute("message", "Yor are logged out.");
+            } else {
+                response.sendRedirect("home");
+                return;
+            }
+        }
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        if (username == null || username.isEmpty() || password == null || password.isEmpty()) {
+            request.setAttribute("message", "Username or password is missing.");
+        } else {
+            AccountService account = new AccountService();
+            
+            User user = account.login(username, password);
+            
+            if (user != null) { //redirect
+                request.getSession().setAttribute("username", username);
+                
+                response.sendRedirect("home");
+                return;
+            } else { //invalid
+                request.setAttribute("username", username);
+                request.setAttribute("message", "Username or password is invalid.");
+            }
+        }
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+    }
+
+}
